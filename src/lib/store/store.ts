798c05a61bfa7baee656e9a4f0bb4d86926dbc19
HoolyDuck <nazar260804@gmail.com>
2024-03-02 19:37:19 +0200

    + api

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from './slices/user.slice';
+import { authApi } from '../apis';
 
 const store = configureStore({
     reducer: {
         [userSlice.name]: userSlice.reducer,
+        [authApi.reducerPath]: authApi.reducer,
     },
 
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(authApi.middleware),
 });
 
 type RootState = ReturnType<typeof store.getState>;
